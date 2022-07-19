63ce1fb3fc9fcc39e06b2ea44dfca13005bd8c3b
AntonMeta <antoni060507@gmail.com>
2022-07-19 10:06:34 +0200

    guzik troche upo bo nie dziala

--- app.js
+++ app.js
@@ -15,3 +15,15 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+var menu = document.getElementById("navBtn")
+function openNav() {
+    if (menu.classList.contains('active')) {
+        document.getElementById("myNav").style.height = "100%";
+        console.log('Yahhh! Class is found.')
+    }
+    else {
+        document.getElementById("myNav").style.height = "0%";
+        console.log('not this time')
+    }
+}
