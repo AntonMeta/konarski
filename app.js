46fd7d1221089894b23350399ba2ed13b35d6a3b
AntonMeta <antoni060507@gmail.com>
2022-07-26 23:33:24 +0200

    zrobiłem w chuj

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var button_up = document.getElementsByClassName("ScrollUpBtn")[0];
 
 window.onscroll = function () { scrollFunction() };
 
+function mobi() {
+    window.open("https://pz1oswiecim.mobidziennik.pl/dziennik/planlekcji?typ=podstawowy", "_blank")
+}
+
 function scrollFunction() {
     if (document.documentElement.scrollTop > 50) {
         button_up.style.display = "block";
@@ -31,28 +35,28 @@ function openNav() {
         document.getElementsByClassName("overlay-sidebar")[0].style.visibility = "hidden";
 
         for (var i = 1; i <= 2; i++) {
-            if (document.getElementById("child"+''+i).classList.contains("active")) {
-                document.getElementById("child"+''+i).classList.toggle("active");
+            if (document.getElementById("child" + '' + i).classList.contains("active")) {
+                document.getElementById("child" + '' + i).classList.toggle("active");
             }
         }
 
         for (var i = 1; i <= 1; i++) {
-            if (document.getElementById("grandchild"+''+i).classList.contains("active")) {
-                document.getElementById("grandchild"+''+i).classList.toggle("active");
+            if (document.getElementById("grandchild" + '' + i).classList.contains("active")) {
+                document.getElementById("grandchild" + '' + i).classList.toggle("active");
             }
         }
 
         var active_element = document.getElementsByClassName("span_first");
-        for(var i = 0; i < active_element.length; i++) {
-            if(active_element[i].classList.contains("active")) {
+        for (var i = 0; i < active_element.length; i++) {
+            if (active_element[i].classList.contains("active")) {
                 active_element[i].classList.toggle("active");
-            }else{
+            } else {
                 active_element[i].classList.toggle("noactive");
             }
         }
 
         var btn = document.getElementsByClassName("anim");
-        for (var i = 0; i < 6; i++) {
+        for (var i = 0; i < 5; i++) {
             btn[i].classList.remove("animate__animated", "animate__fadeInDown");
         }
     }
@@ -76,55 +80,55 @@ window.initMap = initMap;
 function setActive(class_name, index) {
     if (class_name != "none") {
         var active_element = document.getElementsByClassName(class_name);
-        for(var i = 0; i < active_element.length; i++) {
+        for (var i = 0; i < active_element.length; i++) {
             active_element[i].classList.toggle("noactive");
         }
-        active_element[index-1].classList.toggle("noactive"); 
-        active_element[index-1].classList.toggle("active"); 
+        active_element[index - 1].classList.toggle("noactive");
+        active_element[index - 1].classList.toggle("active");
     }
 }
 
 function showChilds(child_type, index, class_active_name, class_anim_name) {
-     // deleting old children from screen
-     var over1ay_sidebar_children = document.getElementsByClassName("overlay-sidebar-"+child_type+"ren");    
-     for (var i = 0; i < over1ay_sidebar_children.length; i++ ){
-        if(over1ay_sidebar_children[i].classList.contains("active")){
+    // deleting old children from screen
+    var over1ay_sidebar_children = document.getElementsByClassName("overlay-sidebar-" + child_type + "ren");
+    for (var i = 0; i < over1ay_sidebar_children.length; i++) {
+        if (over1ay_sidebar_children[i].classList.contains("active")) {
             // checking if not same
-            if (i != index-1) {
-                setActive(class_active_name, i+1);
+            if (i != index - 1) {
+                setActive(class_active_name, i + 1);
                 over1ay_sidebar_children[i].classList.toggle("active");
             }
             break;
         }
-     }
+    }
 
     // deleting grandchildren if sidebar is off
     if (child_type == "child") {
-        var over1ay_sidebar_grandchildren = document.getElementsByClassName("overlay-sidebar-grandchildren");    
-        for(var i = 0; i < over1ay_sidebar_grandchildren.length; i++) {
-            if(over1ay_sidebar_grandchildren[i].classList.contains("active")){
+        var over1ay_sidebar_grandchildren = document.getElementsByClassName("overlay-sidebar-grandchildren");
+        for (var i = 0; i < over1ay_sidebar_grandchildren.length; i++) {
+            if (over1ay_sidebar_grandchildren[i].classList.contains("active")) {
                 over1ay_sidebar_grandchildren[i].classList.toggle("active");
             }
-        } 
-        var active = document.getElementsByClassName("mother");    
-        for(var i = 0; i < active.length; i++) {
-            if(active[i].classList.contains("active")){
+        }
+        var active = document.getElementsByClassName("mother");
+        for (var i = 0; i < active.length; i++) {
+            if (active[i].classList.contains("active")) {
                 active[i].classList.toggle("active");
             }
-        } 
+        }
     }
 
-     // showing children if different, deleting if same
-     document.getElementById(child_type+''+index).classList.toggle("active");
+    // showing children if different, deleting if same
+    document.getElementById(child_type + '' + index).classList.toggle("active");
 
-     // making element active
-     setActive(class_active_name, index);
-     animateCSS(class_anim_name, 'fadeInLeft');
+    // making element active
+    setActive(class_active_name, index);
+    animateCSS(class_anim_name, 'fadeInLeft');
 }
 
 function slideIn() {
     var btn = document.getElementsByClassName("anim");
-    for (var i = 0; i < 6; i++) {
+    for (var i = 0; i < 5; i++) {
         btn[i].classList.add("animate__animated", "animate__fadeInDown");
     }
 }
@@ -133,23 +137,23 @@ function scrollDown() {
 }
 
 const animateCSS = (element, animation, prefix = 'animate__') =>
-  // We create a Promise and return it
-  new Promise((resolve, reject) => {
-    const animationName = `${prefix}${animation}`;
-    const node = document.getElementsByClassName(element);
+    // We create a Promise and return it
+    new Promise((resolve, reject) => {
+        const animationName = `${prefix}${animation}`;
+        const node = document.getElementsByClassName(element);
 
-    for (var i = 0; i < node.length; i++){
-        node[i].classList.add(`${prefix}animated`, animationName);
-    }
+        for (var i = 0; i < node.length; i++) {
+            node[i].classList.add(`${prefix}animated`, animationName);
+        }
 
-    // When the animation ends, we clean the classes and resolve the Promise
-    function handleAnimationEnd(event) {
-      event.stopPropagation();
-      for (var i = 0; i < node.length; i++){
-        node[i].classList.remove(`${prefix}animated`, animationName);
-      }
-      resolve('Animation ended');
-    }
+        // When the animation ends, we clean the classes and resolve the Promise
+        function handleAnimationEnd(event) {
+            event.stopPropagation();
+            for (var i = 0; i < node.length; i++) {
+                node[i].classList.remove(`${prefix}animated`, animationName);
+            }
+            resolve('Animation ended');
+        }
 
-    node[0].addEventListener('animationend', handleAnimationEnd, {once: true});
-  });
+        node[0].addEventListener('animationend', handleAnimationEnd, { once: true });
+    });
